f230604f79de21e76bb71eff7de8bddf01fe645b
Keith Filkins <kafilkins@me.com>
2020-12-18 21:30:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,9 +19,7 @@ def initialize(name)
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    add_post(post)
   end
 
   def self.post_count

820a98804412dd8996341d5f35131d17d2706277
Keith Filkins <kafilkins@me.com>
2020-12-18 21:31:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
 attr_accessor :name
-@@post_count = 0
+
 
 def initialize(name)
   @name = name
@@ -23,6 +23,6 @@ def initialize(name)
   end
 
   def self.post_count
-    @@post_count
+    
   end
 end
